e6360858e1f5b2ee176bb99b5725b194417789dd
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-10 22:23:02 +0600

    add loobook

--- app/(home)/lookbook/page.jsx
+++ app/(home)/lookbook/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <section>
+        <div className="">
+          <div className="">
+            <h2 className="">TERMS & CONDITIONS</h2>
+            <h5 className="">Your use of this is governed by these terms of use</h5>
+            <p className="">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default page;
