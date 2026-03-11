db35fc656d09d4440e90cb74092343176f873ddf
menabil <nabil99cc@gmail.com>
2026-03-11 22:55:07 +0600

    add tracking page

--- app/(home)/tracking/page.jsx
+++ app/(home)/tracking/page.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const page = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col items-center justify-center bg-white px-6 py-12">
-      <div className="w-full max-w-150 text-center">
+    <section className="min-h-[80vh] flex flex-col items-center justify-center bg-white py-22.5">
+      <div className="w-full max-w-125 text-center">
 
         {/* Heading */}
-        <h3 className="text-[35px] md:text-2xl font-bold leading-12.75 text-[#222222] uppercase mb-6">
+        <h3 className="text-[35px] md:text-2xl font-bold leading-12.75 text-[#222222] uppercase mb-8">
           Order Tracking
         </h3>
 
         {/* Instructions */}
-        <p className="text-[#777] text-sm md:text-base leading-relaxed mb-10 max-w-125 mx-auto">
+        <p className="text-[#767676] text-sm md:text-xs leading-6 mb-5 max-w-105 mx-auto">
           To track your order please enter your Order ID in the box below and press the "Track" button. This was given to you on your receipt and in the confirmation email you should have received.
         </p>
 
@@ -25,15 +25,15 @@ const page = () => {
               name="orderId"
               placeholder="Order ID"
               required
-              className="w-full border border-gray-300 p-4 text-sm focus:outline-none focus:border-black placeholder:text-gray-400"
+              className="w-full border-2 border-[#E4E4E4] p-4 text-sm focus:outline-none focus:border-black placeholder:text-[#767676]"
             />
           </div>
 
           {/* Billing Email Input with Border Label */}
-          <div className="relative mt-8">
+          <div className="relative mt-7.5 mb-0">
             <label
               htmlFor="email"
-              className="absolute -top-2.5 left-4 bg-white px-2 text-[11px] uppercase font-bold text-gray-800 tracking-wider"
+              className="absolute -top-3 left-4 bg-white pl-1 pr-3 text-sm leading-6 text-[#222222]"
             >
               Billing email
             </label>
@@ -43,14 +43,14 @@ const page = () => {
               name="email"
               placeholder="Email address"
               required
-              className="w-full border-2 border-black p-4 text-sm focus:outline-none"
+              className="w-full border-2 border-[#222222] p-4 text-sm focus:outline-none"
             />
           </div>
 
           {/* Track Button */}
           <button
             type="submit"
-            className="w-full bg-[#1c1c1c] text-white font-bold py-5 mt-4 uppercase tracking-[0.2em] hover:bg-black transition-all duration-200 text-sm"
+            className="w-full bg-[#222222] text-white font-medium py-5 mt-6 uppercase leading-6 text-sm"
           >
             Track
           </button>
